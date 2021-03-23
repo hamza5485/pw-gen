0c4b3abeaf2092dd500a78ace23e7ea90e6677e0
Hamza Anas <hamza.anas@itinnovate.com.au>
2021-03-23 23:37:25 +0500

    card styles

---
+++ src/styles/card.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core'
+
+const theme = createMuiTheme()
+
+export const cardRoot = {
+    maxWidth: 1080,
+    minWidth: 280,
+    textAlign: '-webkit-center',
+    margin: '1em'
+};
