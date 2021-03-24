8403539b14755b19a9aae3966197f91fe7e6a314
Hamza Anas <hamza.anas@itinnovate.com.au>
2021-03-24 15:42:57 +0500

    mainview implementation of snackbar

--- src/views/MainView.jsx
+++ src/views/MainView.jsx
@@ -2,6 +2,7 @@ import { Grid, makeStyles } from '@material-ui/core';
 import React from 'react';
 import ActionCard from '../components/app/ActionCard';
 import HeadingCard from '../components/app/HeadingCard';
+import Snackbar from '../components/app/Snackbar';
 import PassphraseGenerator from '../lib/PassphraseGenerator';
 
 const useStyles = makeStyles(theme => ({
@@ -15,6 +16,17 @@ const passGen = new PassphraseGenerator();
 const MainView = () => {
     const classes = useStyles();
     const [password, setPassword] = React.useState('');
+    const [open, setOpen] = React.useState(false);
+
+    const handleClick = () => setOpen(true);
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
 
     const generatePassword = async () => {
         setPassword('');
@@ -26,9 +38,9 @@ const MainView = () => {
         <Grid container spacing={0} direction="column" alignItems="center" justify="center" className={classes.container}>
             <Grid item xs={12}>
                 <HeadingCard heading="Password Generator" />
-                <ActionCard data={password} body={"click to copy"} onClickAction={generatePassword}/>
+                <ActionCard data={password} body={"Click to copy"} onClickAction={generatePassword} generateNotification={handleClick} />
+                <Snackbar message="Copied to cliboard!" open={open} handleClose={handleClose}/>
             </Grid>
-
         </Grid>
     );
 };
