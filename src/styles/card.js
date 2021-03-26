38fdc8bad6e5b581967c78561dd51e465986df98
Hamza Anas <hamza.anas@itinnovate.com.au>
2021-03-26 20:32:31 +0500

    updated responsive styles (galaxy fold)

--- src/styles/card.js
+++ src/styles/card.js
@@ -4,13 +4,15 @@ const theme = createMuiTheme()
 
 export const cardRoot = {
     maxWidth: 1080,
-    minWidth: 800,
     textAlign: '-webkit-center',
     margin: '1em',
-    [theme.breakpoints.down('xs')]: {
-        minWidth: 280
+    [theme.breakpoints.down('sm')]: {
+        minWidth: 250
     },
     [theme.breakpoints.up('sm')]: {
-        minWidth: 600
+        minWidth: 500
     },
+    [theme.breakpoints.up('md')]: {
+        minWidth: 800
+    }
 };
